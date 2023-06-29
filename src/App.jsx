a4c8a9f10571bdf8a9e50a77fb2481a84f234ce0
Dawit Gashaw <yepdawit@gmail.com>
2023-06-29 17:04:31 -0400

    fixed app data and css

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,30 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  function handleFeatPupClick(id) {
+    setFeatPupId(id);
+  }
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   return (
     <div className="App">
-      {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
-      })}
+      {puppies.map((puppy) => (
+        <p key={puppy.id} onClick={() => handleFeatPupClick(puppy.id)}>
+          {puppy.name}
+        </p>
+      ))}
+
+      {featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>{featuredPup.age}</li>
+            <li>{featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
